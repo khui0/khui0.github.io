716335fcd973d63bced2a3551231ecc36a029799
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-29 12:55:43 -0400

    Add web development section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,10 +27,51 @@
 
 <div class="m-10">
   <h2 class="text-5xl">Web Development</h2>
-  <p class="text-neutral-content mb-5">
+  <p class="text-neutral-content my-5">
     Some of my work for the World Wide Web
   </p>
-  <div class="grid grid-cols-1 lg:grid-cols-2">
-    
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+    <enhanced:img
+      src="$lib/assets/virtual-clicker.png"
+      alt=""
+      class="object-cover"
+    />
+    <div class="prose lg:prose-2xl">
+      <h3 class="font-normal">Virtual Clicker</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        facilis delectus doloremque fugiat dicta temporibus consequuntur
+        perspiciatis ea ab quaerat minus ut fuga, numquam nihil, pariatur
+        quibusdam! Quia, molestias sunt.
+      </p>
+    </div>
+    <div class="prose lg:prose-2xl">
+      <h3 class="font-normal">Falcon Report Graphics</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        facilis delectus doloremque fugiat dicta temporibus consequuntur
+        perspiciatis ea ab quaerat minus ut fuga, numquam nihil, pariatur
+        quibusdam! Quia, molestias sunt.
+      </p>
+    </div>
+    <enhanced:img
+      src="$lib/assets/fr-graphics.png"
+      alt=""
+      class="object-cover"
+    />
+    <enhanced:img
+      src="$lib/assets/bell-tracker.png"
+      alt=""
+      class="object-cover"
+    />
+    <div class="prose lg:prose-2xl">
+      <h3 class="font-normal">Bell Tracker</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        facilis delectus doloremque fugiat dicta temporibus consequuntur
+        perspiciatis ea ab quaerat minus ut fuga, numquam nihil, pariatur
+        quibusdam! Quia, molestias sunt.
+      </p>
+    </div>
   </div>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { webfontDownload } from "vite-plugin-webfont-dl";
+import { enhancedImages } from "@sveltejs/enhanced-img";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     webfontDownload(),
+    enhancedImages(),
   ],
 });
