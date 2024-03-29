f7168a4990531ecd5a255809af62694c48090777
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-29 12:41:33 -0400

    Add grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,9 +18,19 @@
         Kenny Hui
       </h1>
       <p class="py-6 text-neutral-content">
-        Hi! I'm a high school student with a passion for coding. Experienced
-        in web development, and 3D printing.
+        Hi! I'm a high school student with a passion for coding. Experienced in
+        web development, and 3D printing.
       </p>
     </div>
+  </div>
+</div>
+
+<div class="m-10">
+  <h2 class="text-5xl">Web Development</h2>
+  <p class="text-neutral-content mb-5">
+    Some of my work for the World Wide Web
+  </p>
+  <div class="grid grid-cols-1 lg:grid-cols-2">
+    
   </div>
 </div>
